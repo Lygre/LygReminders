e63fc0ef89c8e6ac24e29f054df5e8a2d23ce695
Hugh <hughdbroome@icloud.com>
2019-08-09 11:18:05 -0400

    Initial Commit

---
+++ LygReminders/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LygReminders
+//
+//  Created by Hugh Broome on 8/9/19.
+//  Copyright © 2019 Lygre Tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ LygRemindersTests/LygRemindersTests.swift
@@ -0,0 +1,34 @@
+//
+//  LygRemindersTests.swift
+//  LygRemindersTests
+//
+//  Created by Hugh Broome on 8/9/19.
+//  Copyright © 2019 Lygre Tech. All rights reserved.
+//
+
+import XCTest
+@testable import LygReminders
+
+class LygRemindersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
