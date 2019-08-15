dfc8287c0f4681e494a37626fe6daae2b5b4070d
Hugh <hughdbroome@icloud.com>
2019-08-15 12:35:49 -0400

    -- Not entirely sure what was going on. For some reason system Images were not loading. Resolved now inexplicably

--- LygReminders/View/RenderingViews/RemindersHomeView.swift
+++ LygReminders/View/RenderingViews/RemindersHomeView.swift
@@ -17,66 +17,83 @@ struct RemindersHomeView: View {
     // MARK: Constants & Properties
     
     @State private var searchQueryText = ""
+    @State private var newReminderTitle = ""
     
     @State private var detailCardDisplayed: Bool = true
     
     //MARK: View Body
     var body: some View {
         NavigationView {
-            ScrollView([.vertical]) {
-                VStack {
-                    Section(header: ReminderSearchBarView(searchQueryText: $searchQueryText)) {
-                        // MARK: - Category Filter Panel
-                        // TODO: Make panel and implement
-                        Button(
-                            action: {
-                                withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0)) {
-                                    self.detailCardDisplayed.toggle()
-                                }
-                        },
-                            label: { ZStack(alignment: .center) {
-                                
-                                RoundedRectangle(cornerRadius: 8).fill(Color.gray)
-                                    .padding([.leading, .trailing], 50)
-                                
-                                Text("Placeholder")
-                                    .padding(.vertical, 5)
-                                
-                                }
-                            }
-                        )
+            VStack {
+                Section(header: ReminderSearchBarView(searchQueryText: $searchQueryText)) {
+                    ZStack(alignment: .center) {
+                    HStack(spacing: 5) {
+                        TextField("New Reminder Title", text: $newReminderTitle, onEditingChanged: { _ in }, onCommit: { })
                         
+                        Button(action: { }, label: {
+                              Image(systemName: "folder.badge.plus.fill")
+                        })
+                            .foregroundColor(.green)
                     }
-                    
-                    Section {
-                        GeometryReader { proxy in
-                        ZStack(alignment: .center) {
-                            if self.detailCardDisplayed {
-                                RoundedRectangle(cornerRadius: 8).fill(Color.blue)
-                                    .padding([.leading, .trailing], proxy.size.width / 10)
-                                    .frame(height: proxy.size.width)
-                            
-                                Text("DetailCardPLaceholder")
-                            }
-                        }
-                        .shadow(color: Color.black, radius: 5, x: 5, y: 5)
-                        .transition(.slide)
-                            
-                            
-                        }
                     }
                 }
                     
-                    // MARK: Spacer End Of VStack
-//                    Spacer()
+                
+                ScrollView([.vertical]) {
+//                    VStack {
+                        Section {
+                            // MARK: - Category Filter Panel
+                            // TODO: Make panel and implement
+                            Button(
+                                action: {
+                                    withAnimation(.interpolatingSpring(stiffness: 300.0, damping: 30.0)) {
+                                        self.detailCardDisplayed.toggle()
+                                    }
+                            },
+                                label: { ZStack(alignment: .center) {
+                                    
+                                    RoundedRectangle(cornerRadius: 8).fill(Color.gray)
+                                        .padding([.leading, .trailing], 50)
+                                    
+                                    Text("Placeholder")
+                                        .padding(.vertical, 5)
+                                    
+                                    }
+                            }
+                            )
+                            
+                        }
+                        
+                        Section {
+                            GeometryReader { proxy in
+                                ZStack(alignment: .center) {
+                                    if self.detailCardDisplayed {
+                                        RoundedRectangle(cornerRadius: 8).fill(Color.blue)
+                                            .padding([.leading, .trailing], proxy.size.width / 10)
+                                            .frame(height: proxy.size.width)
+                                        
+                                        Text("DetailCardPLaceholder")
+                                    }
+                                }
+                                .shadow(color: Color.black, radius: 5, x: 5, y: 5)
+                                .transition(.slide)
+                                
+                                
+                            }
+                        }
+                    }
                     
+                    // MARK: Spacer End Of VStack
+                    //                    Spacer()
+                    
+                }
             }
-            .navigationBarTitle("", displayMode: .inline)
+//            .navigationBarTitle("", displayMode: .inline)
             .navigationBarItems(trailing: EditButton())
-            }
             
-        }
+//        .colorScheme(.dark)
     }
+}
 
 
 #if DEBUG
